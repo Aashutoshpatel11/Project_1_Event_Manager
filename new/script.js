541321960251f37feeee6e58d8237ea7245ecaea
Aashutoshpatel11 <146903229+Aashutoshpatel11@users.noreply.github.com>
2024-07-27 14:22:00 +0000

    almost done

--- new/script.js
+++ new/script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(e){
     const slider = document.querySelector('.slider');
     const leftnavbar = document.querySelector('.leftnavbar');
+    const sociallinks = document.querySelector('.sociallinks');
 
     console.log(slider)
     console.log(leftnavbar);
@@ -8,8 +9,10 @@ document.addEventListener('DOMContentLoaded', function(e){
     slider.addEventListener('click', function(e){
         if( leftnavbar.style.width==='270px' ){
             leftnavbar.style.width='75px';
+            sociallinks.style.display='none';
         }else{
             leftnavbar.style.width='270px';
+            sociallinks.style.display='flex';
         }
     } )
 } )
